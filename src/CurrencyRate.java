60b3485c06b95d2c4c81330a7603e1ef940e966c
mateusz_rozanowski <m.rozanowski@stuent.uj.edu.pl>
2022-04-24 20:42:30 +0200

    reading csv

--- src/CurrencyRate.java
+++ src/CurrencyRate.java
@@ -1,10 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class CurrencyRate {
-    private double USDtoPLN = 4.31;
-    private double USDtoEUR = 0.93;
-    private double PLNtoEUR = USDtoEUR/USDtoPLN;
-    private double PLNtoUSD = 1.0/USDtoPLN;
-    private double EURtoUSD = 1.0/USDtoEUR;
-    private double EURtoPLN = 1/PLNtoEUR;
+    private double USDtoPLN;
+    private double USDtoEUR;
+    private double PLNtoEUR;
+    private double PLNtoUSD;
+    private double EURtoUSD;
+    private double EURtoPLN;
+
+    public CurrencyRate() {
+        try {
+            init();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        initRest();
+    }
+
+    private void initRest() {
+        PLNtoEUR = USDtoEUR/USDtoPLN;
+        PLNtoUSD = 1.0/USDtoPLN;
+        EURtoUSD = 1.0/USDtoEUR;
+        EURtoPLN = 1/PLNtoEUR;
+    }
 
     public double getRate(String s){
         return switch (s){
@@ -16,6 +38,19 @@ public class CurrencyRate {
             case "EURtoPLN" -> EURtoPLN;
             default -> -1;
         };
+    }
+
+    private void init() throws FileNotFoundException {
+            String fileName = "currency.csv";
+            Scanner s = new Scanner(new File(fileName));
+            LinkedList<String[]> lines = new LinkedList<>();
+            while (s.hasNext()) {
+                lines.add(s.nextLine().split(","));
+            }
+            s.close();
+            USDtoPLN = Double.parseDouble(lines.get(0)[1]);
+            USDtoEUR = Double.parseDouble(lines.get(1)[1]);
+
     }
 
 }
